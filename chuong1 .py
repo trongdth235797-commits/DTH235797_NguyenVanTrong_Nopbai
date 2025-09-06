0462948fe284e399c457b997d1a2139c6846fad0
trongdth235797-commits <trong_dth235797@student.agu.edu.vn>
2025-09-06 13:02:50 +0700

    init project

---
+++ chuong1 .py
@@ -0,0 +1,26 @@
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print('Chao cac ban!')
+print('Nguyen Van Trong')
+print(' Minh ve minh co nho ta ?')
+print(' Muoi lam nam ay thiet tha man nong')
+print('Minh ve minh co nho khong')
+print('Nhin cay nho nui ,nhin song nho nguon ')
+chuoi = input ('Nhap mot chuoi bat ky:')
+print('chuoi ban vua in:',chuoi)
+print('****')
+print('*  *')
+print('*  *')
+print('****')
+for i in range(4):
+    print('*'*9)
+print('     *     ')
+print('    ***    ')
+print('  *******  ')
+print('    ***    ')
+print('   *****   ')
+print('***********')
+print('    * *    ')
+print('    * *    ')
+
